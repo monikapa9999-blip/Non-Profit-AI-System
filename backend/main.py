bdd63f163f7b60a9cc283a6c1c1b0c66785eae9f
Madhuri Prajapati <monikapa9999@gmail.com>
2026-05-06 23:48:11 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -36,6 +36,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://127.0.0.1:3000",
         "http://13.206.207.12:3000",
+        "http://13.206.207.12:3000"
     ],
     allow_credentials=True,
     allow_methods=["*"],
