6402e655c0ba2a69b216777727a225cf5cd7bb96
Madhuri Prajapati <monikapa9999@gmail.com>
2026-05-06 23:00:19 +0530

    Update apiClient.js

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-baseURL: "http://localhost:8000",
+baseURL: "http://13.206.207.12:8000",
   headers: { "Content-Type": "application/json" },
   timeout: 30000,
 })
